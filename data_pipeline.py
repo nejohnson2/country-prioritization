4a46b55ef7d604d26a10ad440ba4920b53888675
Nicholas Johnson <nejohnson2@users.noreply.github.com>
2025-02-10 20:42:46 -0500

    Create data_pipeline.py

---
+++ data_pipeline.py
@@ -0,0 +1,6 @@
+from functions import *
+import time
+import datetime
+
+print("Starting data pipeline at ", datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+print("----------------------------------------------")
